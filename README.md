fc763e4d2592eca45d29caf11b22fb81e37d973e
nikoltat801 <tetiana.nikolus@student.hamk.fi>
2024-09-11 11:17:43 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Tulostin-ohjelmointiharjoitus-GIT
+ 
---
+++ java/src/App.java
@@ -0,0 +1,53 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hei olen Tulostin-ohjelma");
+        String tekija;
+        tekija = "Tanja";
+
+        System.out.print("Ohjelman tekijä: ");
+        System.out.println(tekija);
+
+        System.out.println("Ohjelman tekijä: " + tekija);
+
+
+
+
+        int luku1; 
+        int luku2;
+        int result;
+        luku1 = 6;
+        luku2 = 2;
+        result = luku1 - luku2;
+        //System.out.println(luku1);
+        
+        System.out.println("Lukun1-muuttujan arvo on " + luku1);
+        System.out.println("Lukun2-muuttujan arvo on " + luku2);
+        System.out.print(luku1 + " - " + luku2 + " = " + result);
+
+
+
+
+        // int luku1; 
+        // int luku2;
+        // int result;
+        // luku1 = 6;
+        // luku2 = 2;
+        // result = luku1 + luku2;
+        // //System.out.println(luku1);
+        
+        // System.out.println("Lukun1-muuttujan arvo on " + luku1);
+        // System.out.println("Lukun2-muuttujan arvo on " + luku2);
+        // System.out.print(luku1 + " + " + luku2 + " = " + result);
+
+
+       
+
+    }
+}
+
+
+
+
+
+
+
