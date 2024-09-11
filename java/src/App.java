356b81596b9b61f09303ed7e776cab808831815e
nikoltat801 <tetiana.nikolus@student.hamk.fi>
2024-09-11 11:29:07 +0300

    int jako ja kertolasku

--- java/src/App.java
+++ java/src/App.java
@@ -12,32 +12,66 @@ public class App {
 
 
 
+        // int luku1; 
+        // int luku2;
+        // int result;
+        // luku1 = 6;
+        // luku2 = 2;
+        // result = luku1 - luku2;
+        // //System.out.println(luku1);
+        
+        // System.out.println("Lukun1-muuttujan arvo on " + luku1);
+        // System.out.println("Lukun2-muuttujan arvo on " + luku2);
+        // System.out.print(luku1 + " - " + luku2 + " = " + result);
+
+
+
+
+        // int numero1; 
+        // int numero2;
+        // int tulos;
+        // numero1 = 9;
+        // numero2 = 2;
+        // tulos = numero1 + numero2;
+        // //System.out.println(luku1);
+        
+        // System.out.println("Lukun1-muuttujan arvo on " + numero1);
+        // System.out.println("Lukun2-muuttujan arvo on " + numero2);
+        // System.out.print(numero1 + " + " + numero2 + " = " + tulos);
+
+
+        
+
+
+
+
+
+        //    int luku1; 
+        // int luku2;
+        // int result;
+        // luku1 = 5;
+        // luku2 = 2;
+        // result = luku1 * luku2;
+        // //System.out.println(luku1);
+        
+        // System.out.println("Lukun1-muuttujan arvo on " + luku1);
+        // System.out.println("Lukun2-muuttujan arvo on " + luku2);
+        // System.out.print(luku1 + " * " + luku2 + " = " + result);
+    
+    
+
+
         int luku1; 
         int luku2;
         int result;
         luku1 = 6;
         luku2 = 2;
-        result = luku1 - luku2;
+        result = luku1 / luku2;
         //System.out.println(luku1);
         
         System.out.println("Lukun1-muuttujan arvo on " + luku1);
         System.out.println("Lukun2-muuttujan arvo on " + luku2);
-        System.out.print(luku1 + " - " + luku2 + " = " + result);
-
-
-
-
-        int numero1; 
-        int numero2;
-        int tulos;
-        numero1 = 9;
-        numero2 = 2;
-        tulos = numero1 + numero2;
-        //System.out.println(luku1);
-        
-        System.out.println("Lukun1-muuttujan arvo on " + numero1);
-        System.out.println("Lukun2-muuttujan arvo on " + numero2);
-        System.out.print(numero1 + " + " + numero2 + " = " + tulos);
+        System.out.print(luku1 + " / " + luku2 + " = " + result);
 
 
        
